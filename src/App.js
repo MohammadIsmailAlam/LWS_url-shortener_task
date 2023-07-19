8258316a98021f3fb2dfd98e5ed6ce36dab33634
mohammadIsamil <ismailalam901@gmail.com>
2023-07-19 12:18:49 +0600

    Entry Page Created where url will be entered

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import EntryPage from './EntryPage';
 
-function App() {
-  return <div className="App">
-    
-  </div>;
-}
+const App = () => {
+  const [shortURLs, setShortURLs] = useState([]);
+
+  useEffect(() => {
+    const storedURLs = localStorage.getItem('shortURLs');
+    if (storedURLs) {
+      setShortURLs(JSON.parse(storedURLs));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('shortURLs', JSON.stringify(shortURLs));
+  }, [shortURLs]);
+
+  const createShortURL = (longURL) => {
+    const shortURL = generateShortURL();
+    setShortURLs([...shortURLs, { shortURL, longURL }]);
+  };
+
+  
+
+  const generateShortURL = () => {
+    // Implement short URL generation logic here
+    // This can involve random string generation or using an external service
+    // For simplicity, let's generate a random 6-character string
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let shortURL = '';
+    for (let i = 0; i < 6; i++) {
+      shortURL += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return shortURL;
+  };
+
+  return (
+    <div>
+      <h1>URL Shortening System</h1>
+      <EntryPage createShortURL={createShortURL} />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/EntryPage.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Button, TextField } from '@material-ui/core';
+
+const EntryPage = ({ createShortURL }) => {
+  const [longURL, setLongURL] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createShortURL(longURL);
+    setLongURL('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Enter a long URL"
+        variant="outlined"
+        value={longURL}
+        onChange={(e) => setLongURL(e.target.value)}
+        fullWidth
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Generate Short URL
+      </Button>
+    </form>
+  );
+};
+
+export default EntryPage;
